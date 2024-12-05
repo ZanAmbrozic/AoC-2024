9aace780d3351428f3f6106ed8c03371e8e36a6a
ZanAmbrozic <zan.ambrozic@gmail.com>
2024-12-05 09:27:51 +0100

    Cleaned and added comment on Day_05

--- Day_05/main.py
+++ Day_05/main.py
@@ -4,22 +4,26 @@ adding = True
 res = 0
 res2 = 0
 for line in open("input.txt"):
-    if line.strip() == "":
+    line = line.strip()
+    if line == "":
         adding = False
         continue
     if adding is True:
-        v, k = line.strip().split("|")
+        v, k = line.split("|")
         conditions[k] = conditions.get(k, set()) | {v}
         continue
 
-    updates = line.strip().split(",")
+    updates = line.split(",")
     used = set()
 
     for update in updates:
+        # check if used is a subset of required updates
+        # if it is, it means that requirements are not met and updates are not in order
         if used < (conditions.get(update, set()) & set(updates)):
             ordered = sorted(updates, key=lambda e: len(set(updates) & conditions.get(e, set())))
             res2 += int(ordered[len(ordered) // 2])
             break
+
         used |= {update}
 
     else:
